49958c60f23ca2a48e5924856dcc23419c8f54e1
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-06 22:34:35 -0300

    Agregue cm

--- src/datos.ts
+++ src/datos.ts
@@ -3,5 +3,5 @@ let dato = document.forms[0].dato;
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click",() => {
-    console.log("La altura de la persona",dato.value);
+    console.log("La altura de la persona",dato.value, "cm");
 })rotulo.innerHTML = "La altura es:";
